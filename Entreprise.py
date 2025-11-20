ac95a02b9a2cc0bdf44b6141982053a9b47d3a23
ANGOULA RAPHAEL <farenogiffareno@gmail.com>
2025-11-20 16:10:17 +0100

    qwerf

--- Entreprise.py
+++ Entreprise.py
@@ -1,90 +1,100 @@
-liste_clients = ["raphael", "Anthony", "Eddy"]
+liste_clients = ["Atangana","Moukoko","Ngono","Essomba"]
 
 def ajouter_client(nom):
     if nom not in liste_clients:
         liste_clients.append(nom)
-        print(f"Le client {nom} a été ajouté à la liste.")
+        depenses[nom] = 0.0
     else:
-        print(f"Le client {nom} existe déjà.")
-def supprimer_client(nom):
-    reponse = input("etes-vous sur de vouloir supprimer le client ? (o/n)")
-    if reponse == "o":
-        if nom in liste_clients:
-            liste_clients.pop(nom, 0)
-            print(f"Le client {nom} a été supprimé de la liste.")
-        else:
-            print(f"Le client {nom} n'existe pas.")
-    else:
-        print("Le client n'a pas été supprimé.")
+        return "Ce client existe déjà." 
 
-def afficher_clients():
-    print("Liste des clients :\n")
-    for client in liste_clients:
-        print(client, sep ="," )
 def rechercher_client(nom):
     if nom in liste_clients:
-        print(f"Le client {nom} existe.")
+        return True
     else:
-        print(f"Le client {nom} n'existe pas.")
-
-def gestion_des_clients():
-    while True:
-        print("Bienvenue dans la gestion des clients")
-        print("Que voulez-vous faire ?")
-        print("1. Ajouter un client")
-        print("2. Supprimer un client")
-        print("3. Afficher la liste des clients")
-        print("4. Rechercher un client")
-        print("5. Quitter")
-        choix = input("Entrez votre choix : ")
-        if choix == "1":
-            nom =input("Entrez le nom du client : ")
-            ajouter_client(nom)
-            reponse = input("voulez vous continuer ? (o/n)")
-            if reponse == "o":
-                continue
-            else:
-                break
-        elif choix == "2":
-            nom = input("Entrez le nom du client : ")
-            supprimer_client(nom)
-
-
-
-
-
-
-for client in liste_clients:
-    depenses[client] = 0
-def ajouter_depense(nom, montant):
-    if nom in depenses:
-        depenses[nom] += montant
+        return False
+def supprimer_client(nom):
+    if nom in liste_clients:
+        liste_clients.remove(nom)
+        del depenses[nom]
     else:
         return "Ce client n'existe pas."
+def menu_clients():
+    while True:
+        print("Menu Clients:")
+        print("1. Ajouter un client")
+        print("2. Rechercher un client")
+        print("3. Supprimer un client")
+        print("4. Afficher tous les clients")
+        print("5. Quitter")
+        choix = input("Choisissez une option: ")
+        
+        if choix == '1':
+            nom = input("Entrez le nom du client à ajouter: ")
+            resultat = ajouter_client(nom)
+            if resultat:
+                print(resultat)
+            else:
+                print(f"Client '{nom}' ajouté avec succès.")
+        
+        elif choix == '2':
+            nom = input("Entrez le nom du client à rechercher: ")
+            if rechercher_client(nom):
+                print(f"Client '{nom}' trouvé.")
+            else:
+                print(f"Client '{nom}' non trouvé.")
+        
+        elif choix == '3':
+            nom = input("Entrez le nom du client à supprimer: ")
+            resultat = supprimer_client(nom)
+            if resultat:
+                print(resultat)
+            else:
+                print(f"Client '{nom}' supprimé avec succès.")
+        
+        elif choix == '4':
+            print("Liste des clients:")
+            for client in liste_clients:
+                print(client)
+        
+        elif choix == '5':
+            print("Quitter le menu clients.")
+            break
+        
+        else:
+            print("Option invalide. Veuillez réessayer.")
+
+depenses = {}
+for client in liste_clients:
+    depenses[client] = 0.0
+def ajouter_achat(nom, montant):
+    if nom in depenses :
+        for i in range (0,3):
+            produit = input("Entrez le nom du produit acheté:")
+            montant_produit = float(input("Entrez le montant du produit acheté en EUR:"))
+            depenses[nom] += montant_produit
+            print(f"Achat ajouté: {produit} pour {montant_produit} EUR")
+            print(f"Dépense totale pour {nom}: {depenses[nom]} EUR")   
+    else:
+        print("Client non trouvé.")
 def afficher_depenses():
     print("Dépenses des clients:")
     for client, montant in depenses.items():
-        print(f"{client}: {montant} unités")
+        print(f"{client}: {montant} EUR")  
 def menu_depenses():
     while True:
-        print("\nMenu Dépenses:")
+        print("Menu Dépenses:")
         print("1. Ajouter une dépense")
         print("2. Afficher les dépenses")
         print("3. Quitter")
         choix = input("Choisissez une option: ")
         
         if choix == '1':
-            nom = input("Entrez le nom du client: ")
-            try:
-                montant = float(input("Entrez le montant de la dépense: "))
-                resultat = ajouter_depense(nom, montant)
-                if resultat:
-                    print(resultat)
-                else:
-                    print(f"Dépense de {montant} unités ajoutée pour le client '{nom}'.")
-            except ValueError:
-                print("Montant invalide. Veuillez entrer un nombre.")
-        
+            nom = input("Entrez le nom du client pour ajouter une dépense: ")
+            if nom in liste_clients:
+                ajouter_achat(nom, 0)
+            else:
+                print("Client non trouvé.")        
+
         elif choix == '2':
             afficher_depenses()
         
@@ -94,14 +104,6 @@ def menu_depenses():
         
         else:
             print("Option invalide. Veuillez réessayer.")
-
-
-
-
-
-
-
-
 
 
 
